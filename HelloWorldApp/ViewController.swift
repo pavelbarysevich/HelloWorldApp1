fa4dcfef66e92c3c6f17e3682ee8ed3fa9edd6bd
Pavel Barysevich <pavelbarysevich@gmail.com>
2020-03-09 14:47:07 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Павел Борисевич on 3/9/20.
+//  Copyright © 2020 Павел Борисевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
